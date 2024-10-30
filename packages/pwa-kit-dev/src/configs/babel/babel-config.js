607fac001be9cd6050a597b45eba8628a8df5cdd
Ben Chypak <bchypak@salesforce.com>
2024-10-30 15:23:30 -0700

    Disable babel plugin for tests

--- packages/pwa-kit-dev/src/configs/babel/babel-config.js
+++ packages/pwa-kit-dev/src/configs/babel/babel-config.js
@@ -9,54 +9,61 @@
 import {getApplicationExtensionInfo} from '@salesforce/pwa-kit-extension-sdk/shared/utils'
 import {getConfig} from '@salesforce/pwa-kit-runtime/utils/ssr-config'
 
-const config = {
-    sourceType: 'unambiguous',
-    presets: [
-        [
-            require('@babel/preset-env'),
-            {
-                targets: {
-                    node: 18
+export default (api) => {
+    // Caches the configuration
+    api.cache(true)
+
+    // NOTE: We don't want to use certain plugins when testing.
+    const isJest = process.env.NODE_ENV === 'test' || process.env.JEST_WORKER_ID !== undefined
+
+    return {
+        sourceType: 'unambiguous',
+        presets: [
+            [
+                require('@babel/preset-env'),
+                {
+                    targets: {
+                        node: 18
+                    }
                 }
-            }
+            ],
+            require('@babel/preset-typescript'),
+            require('@babel/preset-react')
         ],
-        require('@babel/preset-typescript'),
-        require('@babel/preset-react')
-    ],
-    plugins: [
-        [
-            require('@salesforce/pwa-kit-extension-sdk/configs/babel/plugin-application-extensions'),
-            {
-                target: 'node',
-                ...getApplicationExtensionInfo(getConfig())
+        plugins: [
+            ...(!isJest ? [
+                require('@salesforce/pwa-kit-extension-sdk/configs/babel/plugin-application-extensions'),
+                {
+                    target: 'node',
+                    ...getApplicationExtensionInfo(getConfig())
+                }
+            ] : []),
+            require('@babel/plugin-transform-async-to-generator'),
+            require('@babel/plugin-proposal-object-rest-spread'),
+            require('@babel/plugin-transform-object-assign'),
+            [
+                require('@babel/plugin-transform-runtime'),
+                {
+                    regenerator: true
+                }
+            ],
+            require('@babel/plugin-syntax-dynamic-import'),
+            require('@loadable/babel-plugin'),
+            require('@babel/plugin-proposal-optional-chaining'),
+            [
+                require('babel-plugin-formatjs'),
+                {
+                    idInterpolationPattern: '[sha512:contenthash:base64:6]',
+                    ast: true
+                }
+            ],
+            require('@babel/plugin-transform-async-generator-functions')
+        ].filter(Boolean),
+        env: {
+            test: {
+                presets: [require('@babel/preset-env'), require('@babel/preset-react')],
+                plugins: [require('babel-plugin-dynamic-import-node-babel-7')]
             }
-        ],
-        require('@babel/plugin-transform-async-to-generator'),
-        require('@babel/plugin-proposal-object-rest-spread'),
-        require('@babel/plugin-transform-object-assign'),
-        [
-            require('@babel/plugin-transform-runtime'),
-            {
-                regenerator: true
-            }
-        ],
-        require('@babel/plugin-syntax-dynamic-import'),
-        require('@loadable/babel-plugin'),
-        require('@babel/plugin-proposal-optional-chaining'),
-        [
-            require('babel-plugin-formatjs'),
-            {
-                idInterpolationPattern: '[sha512:contenthash:base64:6]',
-                ast: true
-            }
-        ],
-        require('@babel/plugin-transform-async-generator-functions')
-    ].filter(Boolean),
-    env: {
-        test: {
-            presets: [require('@babel/preset-env'), require('@babel/preset-react')],
-            plugins: [require('babel-plugin-dynamic-import-node-babel-7')]
         }
     }
 }
-export default config
