85cf61df36f1d76b8e81d8e160ff4c881631ba59
Adam Raya Navarro <arayanavarro@salesforce.com>
2024-11-08 15:09:23 -0800

    Add docs

--- packages/pwa-kit-extension-sdk/src/configs/babel/utils.ts
+++ packages/pwa-kit-extension-sdk/src/configs/babel/utils.ts
@@ -8,6 +8,22 @@ import * as fse from 'fs-extra'
 import * as p from 'path'
 import {getApplicationExtensionInfo} from '../../shared/utils/extensibility'
 
+/**
+ * Constants used for building Babel extensibility arguments:
+ *
+ * IGNORE_PATH: Provides a placeholder path that does not exist, used in Babel's `--ignore`
+ * option to prevent processing of any unintended files by specifying a path that cannot match
+ * any actual file. This effectively disables Babel's ignore behavior when no other paths are specified.
+ *
+ * SERVER_PATH: Absolute path to the server entry point (`build-remote-server.js`) within the
+ * `@salesforce/pwa-kit-runtime` package. Required for Babel processing to ensure that server-side
+ * code, especially for SSR, is correctly transpiled as part of the extensibility setup.
+ *
+ * PLACEHOLDER_PATH: Absolute path to `application-extensions.js`, a placeholder file in the
+ * `@salesforce/pwa-kit-extension-sdk` package. This file acts as a stand-in for extensions
+ * that may or may not be present, allowing Babel to process extensibility placeholders even
+ * if no actual extensions are added.
+ */
 const NODE_MODULES_PATH = 'node_modules'
 const IGNORE_PATH = `${NODE_MODULES_PATH}/does_not_exist`
 const SERVER_PATH = `${NODE_MODULES_PATH}/@salesforce/pwa-kit-runtime/ssr/server/build-remote-server.js`
