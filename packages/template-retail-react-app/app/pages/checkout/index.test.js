532c84cb62067435bd7586cad5e1c4d7540c1684
Kevin He <kevin.he@salesforce.com>
2025-01-14 09:18:00 -0800

    try to fix ci error 2

--- packages/template-retail-react-app/app/pages/checkout/index.test.js
+++ packages/template-retail-react-app/app/pages/checkout/index.test.js
@@ -394,7 +394,7 @@ test('Can proceed through checkout steps as guest', async () => {
     // Fill out credit card payment form
     await user.type(screen.getByLabelText(/card number/i), '4111111111111111')
     await user.type(screen.getByLabelText(/name on card/i), 'Testy McTester')
-    await user.type(screen.getByLabelText(/expiration date/i), '1224')
+    await user.type(screen.getByLabelText(/expiration date/i), '1240')
     await user.type(screen.getByLabelText(/^security code$/i /* not "security code info" */), '123')
 
     // Same as shipping checkbox selected by default
@@ -417,7 +417,7 @@ test('Can proceed through checkout steps as guest', async () => {
     // Verify applied payment and billing address
     expect(step3Content.getByText('Visa')).toBeInTheDocument()
     expect(step3Content.getByText('•••• 1111')).toBeInTheDocument()
-    expect(step3Content.getByText('12/2024')).toBeInTheDocument()
+    expect(step3Content.getByText('12/2040')).toBeInTheDocument()
 
     expect(step3Content.getByText('Tester McTesting')).toBeInTheDocument()
     expect(step3Content.getByText('123 Main St')).toBeInTheDocument()
@@ -485,7 +485,7 @@ test('Can proceed through checkout as registered customer', async () => {
     // (we no longer have saved payment methods)
     await user.type(screen.getByLabelText(/card number/i), '4111111111111111')
     await user.type(screen.getByLabelText(/name on card/i), 'Testy McTester')
-    await user.type(screen.getByLabelText(/expiration date/i), '1224')
+    await user.type(screen.getByLabelText(/expiration date/i), '1240')
     await user.type(screen.getByLabelText(/^security code$/i /* not "security code info" */), '123')
 
     // Same as shipping checkbox selected by default
@@ -505,7 +505,7 @@ test('Can proceed through checkout as registered customer', async () => {
     // Verify applied payment and billing address
     expect(step3Content.getByText('Master Card')).toBeInTheDocument()
     expect(step3Content.getByText('•••• 5454')).toBeInTheDocument()
-    expect(step3Content.getByText('1/2030')).toBeInTheDocument()
+    expect(step3Content.getByText('1/2040')).toBeInTheDocument()
 
     expect(step3Content.getByText('123 Main St')).toBeInTheDocument()
 
