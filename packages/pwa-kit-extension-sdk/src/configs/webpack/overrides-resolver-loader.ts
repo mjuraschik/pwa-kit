74b783700d5424770ab9bc551ce664ff1d599dd0
Ben Chypak <bchypak@salesforce.com>
2025-01-17 09:27:32 -0800

    Lint

--- packages/pwa-kit-extension-sdk/src/configs/webpack/overrides-resolver-loader.ts
+++ packages/pwa-kit-extension-sdk/src/configs/webpack/overrides-resolver-loader.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import {LoaderContext, web} from 'webpack'
+import {LoaderContext} from 'webpack'
 import fse from 'fs-extra'
 import os from 'os'
 import path from 'path'
