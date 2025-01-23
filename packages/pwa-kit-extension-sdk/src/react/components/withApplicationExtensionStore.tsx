f4cc9ab5c08522fff2b6c7f0cf8d401485121a04
Ben Chypak <bchypak@salesforce.com>
2025-01-23 13:32:53 -0800

    PR feedback

--- packages/pwa-kit-extension-sdk/src/react/components/withApplicationExtensionStore.tsx
+++ packages/pwa-kit-extension-sdk/src/react/components/withApplicationExtensionStore.tsx
@@ -15,23 +15,35 @@ import {useApplicationExtensionsStore} from '../hooks/useApplicationExtensionsSt
 import {SliceInitializer} from '../hooks/useApplicationExtensionsStore'
 
 // Local Types
-type withStoreOptions = {
+type withApplicationExtensionStoreConfig = {
+    /**
+     * The unique identifier for the slice being added to the store.
+     * This ensures that each extension's state is stored separately and uniquely.
+     */
     id: string
+    /**
+     * The initializer function for the slice.
+     * This function defines the initial state and actions for the slice being added.
+     */
     initializer: SliceInitializer<any>
 }
 
 /**
- * This HOC is used to add a slice to the global store for an extension.
+ * Higher-Order Component (HOC) to add a slice to the global store for an extension.
+ * This allows extensions to define their own state and actions, which are dynamically added to the global Zustand store.
  *
- * @param WrappedComponent
- * @param options
- * @returns
+ * @template C - The props type of the wrapped component.
+ * @param WrappedComponent - The React component to wrap with the HOC.
+ * @param config - Configuration for the HOC.
+ * @param config.id - The unique identifier for the slice being added to the store.
+ * @param config.initializer - The initializer function for defining the slice's state and actions.
+ * @returns  The original component, unmodified.
  */
 const withApplicationExtensionStore = <C,>(
     WrappedComponent: React.ComponentType<C>,
-    options: withStoreOptions
+    config: withApplicationExtensionStoreConfig
 ) => {
-    const {id, initializer} = options
+    const {id, initializer} = config
 
     // Because there extensions have unique slice names, we can safely add them to the global store.
     useApplicationExtensionsStore.getState().addSlice(id, initializer)
