34f33db4af4f2c50321c5a498fd6a446f9ccbd1a
Kevin He <kevin.he@salesforce.com>
2025-03-11 15:16:19 -0700

    rename resetDehydratedStateTimeStamp

--- packages/test-commerce-sdk-react/app/components/_app-config/index.tsx
+++ packages/test-commerce-sdk-react/app/components/_app-config/index.tsx
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import React, {useState, ReactElement} from 'react'
-import {CommerceApiProvider, resetDehydratedQueryTimeStamp} from '@salesforce/commerce-sdk-react'
+import {CommerceApiProvider, resetDehydratedStateTimeStamp} from '@salesforce/commerce-sdk-react'
 import {withReactQuery} from '@salesforce/pwa-kit-react-sdk/ssr/universal/components/with-react-query'
 import {useCorrelationId} from '@salesforce/pwa-kit-react-sdk/ssr/universal/hooks'
 import {proxyBasePath} from '@salesforce/pwa-kit-runtime/utils/ssr-namespace-paths'
@@ -86,7 +86,7 @@ const options = {
             }
         }
     },
-    beforeHydrate: resetDehydratedQueryTimeStamp
+    beforeHydrate: resetDehydratedStateTimeStamp
 }
 
 export default withReactQuery(AppConfig, options)
