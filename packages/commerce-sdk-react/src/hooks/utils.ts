bbbfda52ff75aaae23b494aaec55572c4861c224
Kevin He <kevin.he@salesforce.com>
2025-03-11 08:27:57 -0700

    fix test

--- packages/commerce-sdk-react/src/hooks/utils.ts
+++ packages/commerce-sdk-react/src/hooks/utils.ts
@@ -19,13 +19,14 @@ export const updateCache = (queryClient: QueryClient, cacheUpdates: CacheUpdate,
     cacheUpdates.invalidate?.forEach((invalidate) => {
         // TODO: Fix floating promises (convert updateCache to async)
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
-        queryClient.invalidateQueries({queryKey: invalidate.queryKey})
+        queryClient.invalidateQueries(invalidate)
     })
     cacheUpdates.remove?.forEach((remove) => {
-        queryClient.removeQueries({queryKey: remove.queryKey})
+        queryClient.removeQueries(remove)
     })
     cacheUpdates.update?.forEach((update) =>
         // If an updater isn't given, fall back to just setting the data
+        // @TODO: fix as any
         queryClient.setQueryData(update.queryKey, (update.updater ?? data) as any)
     )
 }
