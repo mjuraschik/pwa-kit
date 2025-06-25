dc48667553f8ad2f6fe1c1f77f2d6cc0759a438e
Larnelle Ankunda <lankunda@salesforce.com>
2025-06-25 10:37:23 -0700

    updated open telemetry files

--- packages/pwa-kit-react-sdk/src/ssr/server/opentelemetry-server.test.js
+++ packages/pwa-kit-react-sdk/src/ssr/server/opentelemetry-server.test.js
@@ -2,7 +2,7 @@
  * @jest-environment node
  */
 /*
- * Copyright (c) 2024, Salesforce, Inc.
+ * Copyright (c) 2025, Salesforce, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
